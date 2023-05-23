068aa9aef87eea2e24bec8b9a566d3a5d2d7cb3f
GrahamKracker <gjguzynski@gmail.com>
2023-05-23 18:07:21 -0400

    Initial Commit

---
+++ Main.cs
@@ -0,0 +1,31 @@
+﻿using BTD_Mod_Helper.Api.ModOptions;
+using CashMultiplier;
+using Il2CppAssets.Scripts.Simulation;
+
+[assembly: MelonInfo(typeof(CashMultiplier.Main), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace CashMultiplier;
+
+[HarmonyPatch]
+public class Main : BloonsTD6Mod
+{
+    private static readonly ModSettingDouble Multiplier = new(.5)
+    {
+        displayName = "Multiplier",
+        description = "The multiplier for all cash gained, cannot go above 1",
+        max = 1,
+    };
+    
+    [HarmonyPatch(typeof(Simulation), nameof(Simulation.AddCash))]
+    [HarmonyPrefix]
+    static void Simulation_AddCash(ref double c, Simulation.CashSource source)
+    {
+        if (Multiplier > 1)
+        {
+            c /= Multiplier;    //troll face
+        }
+        else if (source != Simulation.CashSource.CoopTransferedCash)
+            c *= Multiplier;
+    }
+}
---
+++ ModHelperData.cs
@@ -0,0 +1,11 @@
+﻿namespace CashMultiplier;
+
+public static class ModHelperData
+{
+    public const string Name = "CashMultiplier";
+    public const string Description = "";
+    public const string Version = "1.0.0";
+    public const string RepoOwner = "GrahamKracker";
+    public const string RepoName = "CashMultiplier";
+    public const string WorksOnVersion = "36.3";
+}
