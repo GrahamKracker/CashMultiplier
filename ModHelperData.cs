183e15f5ca520f488095704ceb6b3c5cf74d11d5
GrahamKracker <86011515+GrahamKracker@users.noreply.github.com>
2023-07-27 17:45:18 -0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -7,5 +7,5 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string RepoOwner = "GrahamKracker";
     public const string RepoName = "CashMultiplier";
-    public const string WorksOnVersion = "36.3";
+    public const string WorksOnVersion = "38";
 }
